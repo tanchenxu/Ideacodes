705ead9a74287e4077baed43e4e8a95d1953802d
tanchenxu <1828504053@qq.com>
2022-03-22 23:15:04 +0800

    All

---
+++ idea_java/LanQiaoSuanFa/src/com/akuya/DFS/FullArrangement.java
@@ -0,0 +1,46 @@
+package com.akuya.DFS;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * @author akuya
+ * @create 2022-03-22-21:38
+ */
+public class FullArrangement {
+
+    static List<String> list=new ArrayList<String>();
+    public static void main(String[] args) {
+        Scanner scanner =new Scanner(System.in);
+        int number=scanner.nextInt();
+        int [] Arrays=new int[number];
+        for(int i=0;i<number;i++){
+            Arrays[i]=scanner.nextInt();
+        }
+        StringBuilder str =new StringBuilder();
+        int mery[]=new int[number];
+        dfs(Arrays,str,mery);
+        for(String s:list){
+            System.out.print(s+" ");
+        }
+    }
+
+    public static void dfs(int[] Arrays, StringBuilder str,int mery[] ){
+        if(str.length()==Arrays.length){
+            list.add(str.toString());
+            return;
+        }
+
+        for(int i=0;i<Arrays.length;i++){
+            if(mery[i]==0){
+                str.append(Arrays[i]);
+                mery[i]=1;
+                dfs(Arrays,str,mery);
+                mery[i]=0;
+                str.deleteCharAt(str.length()-1);
+            }
+        }
+    }
+
+}
